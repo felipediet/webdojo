f910af6805c14e6b4b579b0c5613f6401e03a94e
Felipediet <fefola@gmail.com>
2025-05-25 19:11:24 -0300

    Add teste com Mouse Hover

---
+++ web/cypress/e2e/hover.cy.js
@@ -0,0 +1,11 @@
+describe('Hover Example', () => {
+    it('Deve mostrar um texto ao passar o mouse', () => {
+        cy.visitarPortal()
+        cy.fazerLogin('papito@webdojo.com','katana123')
+        
+        cy.contains('Isso é Mouseover!').should('not.exist')
+        cy.get('[data-cy="instagram-link"]').realHover()
+        cy.contains('Isso é Mouseover!').should('exist')
+
+    })
+});
