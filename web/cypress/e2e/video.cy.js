c08c03f1ab6786c57bca91e05b4743d578f40fe7
Felipediet <fefola@gmail.com>
2025-05-25 19:42:41 -0300

    Iframe - Testando elementos em um iFrame

---
+++ web/cypress/e2e/video.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('Video', () => {
+   
+  it('Deve poder tocar o video de exemplo', () => {
+        cy.visitarPortal()
+        cy.fazerLogin('papito@webdojo.com','katana123')
+        cy.contains('Video').click()
+
+        cy.get('iframe[title="Video Player"]')
+            .should('exist')
+            .its('0.contentDocument.body') //Acessa o corpo do primeiro iframe localizado
+            .then(cy.wrap) //Envolve o corpo do iframe em um objeto jQuery
+            .as('iFramePlayer') //Dá um alias para o iframe
+
+        cy.get('@iFramePlayer')
+            .find('.play-button') //Localiza o botão de play
+            .click() //Clica no botão de play
+
+        cy.get('@iFramePlayer')
+            .find('.pause-button') //Localiza o botão de pause
+            .should('be.visible') //Verifica se o botão existe
+        
+
+
+  })
+})
