bc42cf41e8b08981fc50c4d596c1ceb914185930
Felipediet <fefola@gmail.com>
2025-06-22 20:32:06 -0300

    Validação de Diálogos de Confirmação: Lidando com OK e Cancelar
    Utilizado cy.on com as funçoes window:confirm e window:alert

--- web/cypress/e2e/alerts.cy.js
+++ web/cypress/e2e/alerts.cy.js
@@ -5,7 +5,7 @@ describe('Validação de Alertas em JavaScript', () => {
         cy.goTo('Alertas JS', 'JavaScript Alerts');
     });
 
-    it('Deve validar a mensagem de alerta', () => {
+    it.skip('Deve validar a mensagem de alerta', () => {
 
         cy.contains('button', 'Mostrar Alert').click();
 
@@ -16,6 +16,31 @@ describe('Validação de Alertas em JavaScript', () => {
         
     });
 
+    it('Deve confirmar um diálogo e validar a resposta positiva', () => {
+        cy.contains('button', 'Mostrar Confirm').click();
+
+        cy.on('window:confirm', (msg) => {
+            expect(msg).to.equal('Aperta um botão!');
+            return true; // Simula a confirmação do diálogo
+        });
+
+        cy.on('window:alert', (msg) => {
+            expect(msg).to.equal('Você clicou em Ok!');
+        });
+    });
+
+    it('Deve confirmar um diálogo e validar a resposta negativa', () => {
+        cy.contains('button', 'Mostrar Confirm').click();
+        
+        cy.on('window:confirm', (msg) => {
+            expect(msg).to.equal('Aperta um botão!');
+            return false; // Simula a negativa do diálogo
+        });
+
+        cy.on('window:alert', (msg) => {
+            expect(msg).to.equal('Você cancelou!');
+        });
+    });
 
 
 });
