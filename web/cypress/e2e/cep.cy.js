8ca76048fdead256f8a342933b24297ee64ffbfd
Felipediet <fefola@gmail.com>
2025-06-22 21:26:47 -0300

    Interceptando Chamadas Externas. Controle total sobre a resposta da API
    Utilizando intercept para interceptar chamada externa e mockar o resultado

--- web/cypress/e2e/cep.cy.js
+++ web/cypress/e2e/cep.cy.js
@@ -10,9 +10,24 @@ describe('CEP', () => {
     addresses.forEach((address, index) => {
         it(`Deve validar a consulta de CEP - Conjunto ${index + 1}`, () => {
 
+            cy.intercept(
+                'GET'
+                ,`https://viacep.com.br/ws/${address.cep}/json/`,{
+                statusCode: 200,
+                body: {
+                        cep: address.cep,
+                        logradouro: address.street,
+                        bairro: address.neighborhood,
+                        localidade: address.city,
+                        uf: address.state
+                    } // Mockando a resposta da API com os dados do endereço os dados da massa de teste devem ser ajustado conforme o que retorna na requisição, caso constrário a aplicação não vai entender
+            }).as('getCepIntercept'); // Intercepta a requisição para o CEP e cria um alias para a interceptação
+
             cy.get('#cep').type(address.cep);
 
             cy.contains('button', 'Buscar').click();
+
+            cy.wait('@getCepIntercept'); // Aguarda a requisição ser interceptada
 
             cy.get('#street')
                 .should('have.value', address.street);
