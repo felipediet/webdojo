83af2069533dd37d5388500d01944c726b116489
Felipediet <fefola@gmail.com>
2025-07-28 09:53:55 -0300

    Validando Toast do Jeito Certo

--- web/cypress/e2e/expert.cy.js
+++ web/cypress/e2e/expert.cy.js
@@ -3,7 +3,7 @@ describe('Expert Tests', () => {
         cy.visitarPortal();
     });
 
-  it('Deve manipular o valor de um campo', () => {
+    it('Deve manipular o valor de um campo', () => {
     cy.log('todo')
 
     cy.get('#email').invoke('val', 'felipe@test.com')
@@ -18,6 +18,30 @@ describe('Expert Tests', () => {
         .invoke('show')
         .should('be.visible')
 
-  });
+    });
+
+    it('Salvar o DOM com o objeto do tipo TOAST', () => {
+        cy.fazerLogin('papito@webdojo.com', 'katana321')
+
+        // cy.wait(2500)
+
+        // cy.document().then((doc) => {
+        //     cy.writeFile('cypress/downloads/page.html', doc.documentElement.outerHTML)
+        // })
+        //Localizar dentro do arquivo HTML o objeto do tipo TOAST pelo texto do mesmo
+
+        //cy.get('[data-sonner-toaster="true"] div[class=title]')
+            // .should('be.visible')
+            // .should('have.text', 'Acesso negado! Tente novamente.')
+        
+        //cy.get('[data-sonner-toaster="true"] .title')
+            // .should('be.visible')
+            // .should('have.text', 'Acesso negado! Tente novamente.')
+
+        cy.get('[data-sonner-toaster="true"]')
+            .should('be.visible')
+            .find('.title')
+            .should('have.text', 'Acesso negado! Tente novamente.')
+    });
 
 });
